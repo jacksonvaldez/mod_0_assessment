9fbd0c09dcf93b794bf265cbc2570428e340e87e
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-09-24 14:20:20 -0600

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  name: 'Jackson Valdez',
+  school: 'Turing',
+  age: 18,
+  years_of_experience: 0,
+  is_dedicated: true,
+  has_a_job: false,
+  used_tools: ['Google', 'Slack'],
+  things_included_in_routine: ['Personal Care', 'Eating', 'Work on Turing work']
+}
