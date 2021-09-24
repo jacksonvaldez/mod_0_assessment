ba7cafa7033133cf5362ce759c4326ffea5f6b33
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-09-24 14:15:28 -0600

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  name: 'max',
+  type: 't-rex',
+  weight_in_pounds: = 2300,
+  height_in_feet: 30,
+  is_vegetarian: false,
+  is_dangerous: true,
+  edible_foods: ['plants', 'other animals', 'fish'],
+  family_member_names: ['Amilia', 'Cooper']  
+}
