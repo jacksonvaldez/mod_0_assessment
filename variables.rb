a96c1bc15fe731632e496567ad4feba3719bef94
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-09-24 14:08:38 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,16 @@
+movie = 'John Wick'
+is_entertaining = true
+
+movie = 'The Avengers'
+
+num_4 = 50
+num_6 = 500
+
+product = num_4 * num_6
+
+favorite_foods = ['pizza', 'enchiladas', 'tacos']
+
+favorite_foods.pop
+puts favorite_foods
+
+puts movie.length
